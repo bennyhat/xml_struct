a84771662ea6e4bd9edbdf599c6be82d302d221c
Benjamin Brewer <ben@benbrewer.me>
2020-07-14 18:30:06 -0400

    round out basic tests

--- test/unit/xml_struct/deserializer/basic_struct_test.exs
+++ test/unit/xml_struct/deserializer/basic_struct_test.exs
@@ -69,73 +69,105 @@ defmodule XmlStruct.Deserializer.BasicStructTest do
     end
   end
 
-  # describe "DeeplyNestedSimpleStruct.serialize/1" do
-  #   test "serializes nested structs as objects" do
-  #     random_integer = Faker.random_between(1, 10)
-
-  #     assert %{
-  #       "DeeplyNestedFieldOne" => true,
-  #       "DeeplyNestedFieldTwo.NestedFieldOne" => true,
-  #       "DeeplyNestedFieldTwo.NestedFieldThree.member.1.FieldOne" => false,
-  #       "DeeplyNestedFieldTwo.NestedFieldThree.member.2.FieldThree" => random_integer,
-  #       "DeeplyNestedFieldTwo.NestedFieldTwo.FieldTwo" => "goodbye",
-  #       "DeeplyNestedFieldThree.member.1.NestedFieldOne" => true,
-  #       "DeeplyNestedFieldThree.member.1.NestedFieldThree.member.1.FieldOne" => false,
-  #       "DeeplyNestedFieldThree.member.1.NestedFieldThree.member.2.FieldThree" => random_integer,
-  #       "DeeplyNestedFieldThree.member.1.NestedFieldTwo.FieldTwo" => "world",
-  #       "DeeplyNestedFieldThree.member.2.NestedFieldOne" => false,
-  #       "DeeplyNestedFieldThree.member.2.NestedFieldThree.member.1.FieldOne" => true,
-  #       "DeeplyNestedFieldThree.member.2.NestedFieldThree.member.2.FieldThree" => random_integer,
-  #       "DeeplyNestedFieldThree.member.2.NestedFieldTwo.FieldTwo" => "things"
-  #     } == DeeplyNestedSimpleStruct.serialize(
-  #       %DeeplyNestedSimpleStruct{
-  #         deeply_nested_field_one: true,
-  #         deeply_nested_field_two: %NestedSimpleStruct{
-  #           nested_field_one: true,
-  #           nested_field_two: %SimpleStruct{
-  #             field_two: "goodbye"
-  #           },
-  #           nested_field_three: [
-  #             %SimpleStruct{
-  #               field_one: false
-  #             },
-  #             %SimpleStruct{
-  #               field_three: random_integer
-  #             }
-  #           ]
-  #         },
-  #         deeply_nested_field_three: [
-  #           %NestedSimpleStruct{
-  #             nested_field_one: true,
-  #             nested_field_two: %SimpleStruct{
-  #               field_two: "world"
-  #             },
-  #             nested_field_three: [
-  #               %SimpleStruct{
-  #                 field_one: false
-  #               },
-  #               %SimpleStruct{
-  #                 field_three: random_integer
-  #               }
-  #             ]
-  #           },
-  #           %NestedSimpleStruct{
-  #             nested_field_one: false,
-  #             nested_field_two: %SimpleStruct{
-  #               field_two: "things"
-  #             },
-  #             nested_field_three: [
-  #               %SimpleStruct{
-  #                 field_one: true
-  #               },
-  #               %SimpleStruct{
-  #                 field_three: random_integer
-  #               }
-  #             ]
-  #           }
-  #         ]
-  #       }
-  #     )
-  #   end
-  # end
+  describe "DeeplyNestedSimpleStruct.deserialize/1" do
+    test "deserializes nested structs" do
+      assert %DeeplyNestedSimpleStruct{
+        deeply_nested_field_one: true,
+        deeply_nested_field_two: %NestedSimpleStruct{
+          nested_field_one: true,
+          nested_field_two: %SimpleStruct{
+            field_two: "goodbye"
+          },
+          nested_field_three: [
+            %SimpleStruct{
+              field_one: false
+            },
+            %SimpleStruct{
+              field_three: 35
+            }
+          ]
+        },
+        deeply_nested_field_three: [
+          %NestedSimpleStruct{
+            nested_field_one: true,
+            nested_field_two: %SimpleStruct{
+              field_two: "world"
+            },
+            nested_field_three: [
+              %SimpleStruct{
+                field_one: false
+              },
+              %SimpleStruct{
+                field_three: 20
+              }
+            ]
+          },
+          %NestedSimpleStruct{
+            nested_field_one: false,
+            nested_field_two: %SimpleStruct{
+              field_two: "things"
+            },
+            nested_field_three: [
+              %SimpleStruct{
+                field_one: true
+              },
+              %SimpleStruct{
+                field_three: 25
+              }
+            ]
+          }
+        ]
+      } == DeeplyNestedSimpleStruct.deserialize(
+        """
+        <DeeplyNestedSimpleStruct>
+          <DeeplyNestedFieldOne>true</DeeplyNestedFieldOne>
+          <NestedSimpleStruct>
+            <NestedFieldOne>true</NestedFieldOne>
+            <SimpleStruct>
+              <FieldTwo>goodbye</FieldTwo>
+            </SimpleStruct>
+            <SimpleStructs>
+              <member>
+                <FieldOne>false</FieldOne>
+              </member>
+              <member>
+                <FieldThree>35</FieldThree>
+              </member>
+            </SimpleStructs>
+          </NestedSimpleStruct>
+          <NestedSimpleStructs>
+            <member>
+              <NestedFieldOne>true</NestedFieldOne>
+              <SimpleStruct>
+                <FieldTwo>world</FieldTwo>
+              </SimpleStruct>
+              <SimpleStructs>
+                <member>
+                  <FieldOne>false</FieldOne>
+                </member>
+                <member>
+                  <FieldThree>20</FieldThree>
+                </member>
+              </SimpleStructs>
+            </member>
+            <member>
+              <NestedFieldOne>false</NestedFieldOne>
+              <SimpleStruct>
+                <FieldTwo>things</FieldTwo>
+              </SimpleStruct>
+              <SimpleStructs>
+                <member>
+                  <FieldOne>true</FieldOne>
+                </member>
+                <member>
+                  <FieldThree>25</FieldThree>
+                </member>
+              </SimpleStructs>
+            </member>
+          </NestedSimpleStructs>
+        </DeeplyNestedSimpleStruct>
+        """
+      )
+    end
+  end
 end
